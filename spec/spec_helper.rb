96bf0e51613320eb3f6091f7e58e1cc458e8d6f2
Evan Sharp <evan@packetmonkey.org>
2015-01-25 13:58:42 -0500

    Grift some configs from the stock helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,24 @@
-$LOAD_PATH << File.expand_path('../..', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../..', __FILE__)
 
 require 'lib/bag_of_holding'
 
 RSpec.configure do |config|
-  config.disable_monkey_patching!
-  config.order = 'random'
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+  config.warnings = true
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  # config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
 end
